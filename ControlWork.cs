f51bbc93eb51d7724576cc2c5551e5bad019bdb2
Pavel <pashaslovev.@mail.com>
2023-12-11 21:56:15 +1000

    Commit_2:Тело цикла

--- ControlWork.cs
+++ ControlWork.cs
@@ -2,5 +2,12 @@
 string[]array2  =new string[array1.Length];
 void metod ( string []array1, string []array2)
 {
-    
+    int count = 0
+    for(int i = 0;i<array1.Length:i++)
+    {
+        if (array1[i].Length<=3)
+        {
+            array2[count]=array1[i];
+        }
+    }
 }
