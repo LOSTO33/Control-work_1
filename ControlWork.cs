53c4d885bea147537f29ca3a85ec4837f96f1dff
Pavel <pashaslovev.@mail.com>
2023-12-11 21:59:03 +1000

    Commit_3:Вывод данных

--- ControlWork.cs
+++ ControlWork.cs
@@ -8,6 +8,15 @@ void metod ( string []array1, string []array2)
         if (array1[i].Length<=3)
         {
             array2[count]=array1[i];
+            count++;
         }
     }
+}
+void Print (string [] array)
+{
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteL();
 }
